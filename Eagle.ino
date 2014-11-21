01bb9eb67bed41da0077bf0435cc97f8a1ae079f
TheNarcissisticArsonist <TheNarcissisticArsonist@users.noreply.github.com>
2014-11-20 20:28:32 -0500

    Update Eagle.ino

--- Eagle.ino
+++ Eagle.ino
@@ -9,158 +9,165 @@
 #define C A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
-void eagle() {
+unsigned short rB = matrix.Color333(0, 0, 7);
+unsigned short dB = matrix.Color333(0, 0, 1);
+unsigned short cB = matrix.Color333(0, 2, 7);
+unsigned short y = matrix.Color333(7, 7, 0);
+unsigned short rC = matrix.Color333(7, 2, 0);
+unsigned short w = matrix.Color333(0, 0, 0);
+
+void eagle(int c, int r) {
   /*
-  matrix.drawPixel(1, 0, matrix.Color333(0, 0, 7)); //Regular Blue DONE
-  matrix.drawPixel(0, 2, matrix.Color333(0, 0, 1)); //Dark Blue DONE
-  matrix.drawPixel(0, 4, matrix.Color333(0, 2, 7)); //Columbia Blue DONE
-  matrix.drawPixel(4, 5, matrix.Color333(7, 7, 0)); //Yellow DONE
-  matrix.drawPixel(6, 7, matrix.Color333(7, 2, 0)); //Dark Red DONE
-  matrix.drawPixel(6, 3, matrix.Color333(7, 7, 7)); //White
+  matrix.drawPixel(c+1, r+0, rB); //Regular Blue DONE
+  matrix.drawPixel(c+0, r+2, dB); //Dark Blue DONE
+  matrix.drawPixel(c+0, r+4, cB); //Columbia Blue DONE
+  matrix.drawPixel(c+4, r+5, y); //Yellow DONE
+  matrix.drawPixel(c+6, r+7, rC); //Dark Red DONE
+  matrix.drawPixel(c+6, r+3, w); //White
   */
-  matrix.drawPixel(0, 2, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(0, 4, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(0, 6, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(0, 9, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+0, r+2, dB);
+  matrix.drawPixel(c+0, r+4, cB);
+  matrix.drawPixel(c+0, r+6, rB);
+  matrix.drawPixel(c+0, r+9, dB);
   
-  matrix.drawPixel(1, 1, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(1, 3, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(1, 4, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(1, 5, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(1, 6, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(1, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(1, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(1, 9, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+1, r+1, rB);
+  matrix.drawPixel(c+1, r+3, dB);
+  matrix.drawPixel(c+1, r+4, cB);
+  matrix.drawPixel(c+1, r+5, cB);
+  matrix.drawPixel(c+1, r+6, rB);
+  matrix.drawPixel(c+1, r+7, rB);
+  matrix.drawPixel(c+1, r+8, dB);
+  matrix.drawPixel(c+1, r+9, dB);
   
-  matrix.drawPixel(2, 2, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(2, 3, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(2, 4, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(2, 5, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(2, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(2, 8, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+2, r+2, rB);
+  matrix.drawPixel(c+2, r+3, dB);
+  matrix.drawPixel(c+2, r+4, dB);
+  matrix.drawPixel(c+2, r+5, cB);
+  matrix.drawPixel(c+2, r+7, rB);
+  matrix.drawPixel(c+2, r+8, dB);
   
-  matrix.drawPixel(3, 1, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(3, 2, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(3, 3, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(3, 4, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(3, 5, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(3, 6, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(3, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(3, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(3, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(3, 10, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(3, 15, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+3, r+1, cB);
+  matrix.drawPixel(c+3, r+2, cB);
+  matrix.drawPixel(c+3, r+3, rB);
+  matrix.drawPixel(c+3, r+4, dB);
+  matrix.drawPixel(c+3, r+5, dB);
+  matrix.drawPixel(c+3, r+6, cB);
+  matrix.drawPixel(c+3, r+7, rB);
+  matrix.drawPixel(c+3, r+8, dB);
+  matrix.drawPixel(c+3, r+9, rB);
+  matrix.drawPixel(c+3, r+10, cB);
+  matrix.drawPixel(c+3, r+15, y);
   
-  matrix.drawPixel(4, 0, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(4, 5, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(4, 6, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(4, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(4, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(4, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(4, 13, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(4, 14, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+4, r+0, cB);
+  matrix.drawPixel(c+4, r+5, y);
+  matrix.drawPixel(c+4, r+6, dB);
+  matrix.drawPixel(c+4, r+7, rB);
+  matrix.drawPixel(c+4, r+8, dB);
+  matrix.drawPixel(c+4, r+9, rB);
+  matrix.drawPixel(c+4, r+13, rC);
+  matrix.drawPixel(c+4, r+14, y);
   
-  matrix.drawPixel(5, 4, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(5, 5, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(5, 7, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(5, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(5, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(5, 12, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(5, 13, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(5, 14, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(5, 15, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+5, r+4, y);
+  matrix.drawPixel(c+5, r+5, y);
+  matrix.drawPixel(c+5, r+7, dB);
+  matrix.drawPixel(c+5, r+8, dB);
+  matrix.drawPixel(c+5, r+9, rB);
+  matrix.drawPixel(c+5, r+12, rC);
+  matrix.drawPixel(c+5, r+13, rC);
+  matrix.drawPixel(c+5, r+14, y);
+  matrix.drawPixel(c+5, r+15, y);
   
-  matrix.drawPixel(6, 3, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(6, 4, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(6, 5, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(6, 7, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(6, 8, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(6, 9, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(6, 11, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(6, 12, matrix.Color333(7, 2, 0));
+  matrix.drawPixel(c+6, r+3, w);
+  matrix.drawPixel(c+6, r+4, w);
+  matrix.drawPixel(c+6, r+5, y);
+  matrix.drawPixel(c+6, r+7, rC);
+  matrix.drawPixel(c+6, r+8, rC);
+  matrix.drawPixel(c+6, r+9, dB);
+  matrix.drawPixel(c+6, r+11, rC);
+  matrix.drawPixel(c+6, r+12, rC);
   
-  matrix.drawPixel(7, 3, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(7, 4, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(7, 5, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(7, 6, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(7, 7, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(7, 8, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(7, 9, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(7, 10, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(7, 11, matrix.Color333(7, 2, 0));
+  matrix.drawPixel(c+7, r+3, w);
+  matrix.drawPixel(c+7, r+4, cB);
+  matrix.drawPixel(c+7, r+5, w);
+  matrix.drawPixel(c+7, r+6, rC);
+  matrix.drawPixel(c+7, r+7, rC);
+  matrix.drawPixel(c+7, r+8, rC);
+  matrix.drawPixel(c+7, r+9, rC);
+  matrix.drawPixel(c+7, r+10, rC);
+  matrix.drawPixel(c+7, r+11, rC);
   
-  matrix.drawPixel(8, 3, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(8, 4, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(8, 5, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(8, 6, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(8, 7, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(8, 8, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(8, 9, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(8, 10, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(8, 11, matrix.Color333(7, 2, 0));
+  matrix.drawPixel(c+8, r+3, w);
+  matrix.drawPixel(c+8, r+4, w);
+  matrix.drawPixel(c+8, r+5, w);
+  matrix.drawPixel(c+8, r+6, rC);
+  matrix.drawPixel(c+8, r+7, rC);
+  matrix.drawPixel(c+8, r+8, rC);
+  matrix.drawPixel(c+8, r+9, rC);
+  matrix.drawPixel(c+8, r+10, rC);
+  matrix.drawPixel(c+8, r+11, rC);
   
-  matrix.drawPixel(9, 4, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(9, 5, matrix.Color333(7, 7, 7));
-  matrix.drawPixel(9, 6, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(9, 7, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(9, 8, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(9, 9, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(9, 11, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(9, 12, matrix.Color333(7, 2, 0));
+  matrix.drawPixel(c+9, r+4, w);
+  matrix.drawPixel(c+9, r+5, w);
+  matrix.drawPixel(c+9, r+6, rC);
+  matrix.drawPixel(c+9, r+7, rC);
+  matrix.drawPixel(c+9, r+8, rC);
+  matrix.drawPixel(c+9, r+9, dB);
+  matrix.drawPixel(c+9, r+11, rC);
+  matrix.drawPixel(c+9, r+12, rC);
   
-  matrix.drawPixel(10, 7, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(10, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(10, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(10, 12, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(10, 13, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(10, 14, matrix.Color333(7, 7, 0));
-  matrix.drawPixel(10, 15, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+10, r+7, dB);
+  matrix.drawPixel(c+10, r+8, dB);
+  matrix.drawPixel(c+10, r+9, rB);
+  matrix.drawPixel(c+10, r+12, rC);
+  matrix.drawPixel(c+10, r+13, rC);
+  matrix.drawPixel(c+10, r+14, y);
+  matrix.drawPixel(c+10, r+15, y);
   
-  matrix.drawPixel(11, 0, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(11, 6, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(11, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(11, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(11, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(11, 13, matrix.Color333(7, 2, 0));
-  matrix.drawPixel(11, 14, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+11, r+0, cB);
+  matrix.drawPixel(c+11, r+6, dB);
+  matrix.drawPixel(c+11, r+7, rB);
+  matrix.drawPixel(c+11, r+8, dB);
+  matrix.drawPixel(c+11, r+9, rB);
+  matrix.drawPixel(c+11, r+13, rC);
+  matrix.drawPixel(c+11, r+14, y);
   
-  matrix.drawPixel(12, 1, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(12, 2, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(12, 3, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(12, 4, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(12, 5, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(12, 6, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(12, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(12, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(12, 9, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(12, 10, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(12, 15, matrix.Color333(7, 7, 0));
+  matrix.drawPixel(c+12, r+1, cB);
+  matrix.drawPixel(c+12, r+2, cB);
+  matrix.drawPixel(c+12, r+3, rB);
+  matrix.drawPixel(c+12, r+4, dB);
+  matrix.drawPixel(c+12, r+5, dB);
+  matrix.drawPixel(c+12, r+6, cB);
+  matrix.drawPixel(c+12, r+7, rB);
+  matrix.drawPixel(c+12, r+8, dB);
+  matrix.drawPixel(c+12, r+9, rB);
+  matrix.drawPixel(c+12, r+10, cB);
+  matrix.drawPixel(c+12, r+15, y);
   
-  matrix.drawPixel(13, 2, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(13, 3, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(13, 4, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(13, 5, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(13, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(13, 8, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+13, r+2, rB);
+  matrix.drawPixel(c+13, r+3, dB);
+  matrix.drawPixel(c+13, r+4, dB);
+  matrix.drawPixel(c+13, r+5, cB);
+  matrix.drawPixel(c+13, r+7, rB);
+  matrix.drawPixel(c+13, r+8, dB);
   
-  matrix.drawPixel(14, 1, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(14, 3, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(14, 4, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(14, 5, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(14, 6, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(14, 7, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(14, 8, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(14, 9, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+14, r+1, rB);
+  matrix.drawPixel(c+14, r+3, dB);
+  matrix.drawPixel(c+14, r+4, cB);
+  matrix.drawPixel(c+14, r+5, cB);
+  matrix.drawPixel(c+14, r+6, rB);
+  matrix.drawPixel(c+14, r+7, rB);
+  matrix.drawPixel(c+14, r+8, dB);
+  matrix.drawPixel(c+14, r+9, dB);
   
-  matrix.drawPixel(15, 2, matrix.Color333(0, 0, 1));
-  matrix.drawPixel(15, 4, matrix.Color333(0, 2, 7));
-  matrix.drawPixel(15, 6, matrix.Color333(0, 0, 7));
-  matrix.drawPixel(15, 9, matrix.Color333(0, 0, 1));
+  matrix.drawPixel(c+15, r+2, dB);
+  matrix.drawPixel(c+15, r+4, cB);
+  matrix.drawPixel(c+15, r+6, rB);
+  matrix.drawPixel(c+15, r+9, dB);
 }
 
 void setup() {
   matrix.begin();
-  eagle();
+  eagle(0, 0);
 }
 void loop() {
   
