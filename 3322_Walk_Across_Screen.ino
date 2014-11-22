f18cca126cb67a5ff3094d3ff47b3beb507d11e6
TheNarcissisticArsonist <TheNarcissisticArsonist@users.noreply.github.com>
2014-11-22 11:14:58 -0500

    Improved
    Reduced Size
    Made Modular
    Made easier to modify

--- 3322_Walk_Across_Screen.ino
+++ 3322_Walk_Across_Screen.ino
@@ -1,7 +1,6 @@
 #include <Adafruit_GFX.h>
 #include <RGBmatrixPanel.h>
 
-
 #define CLK 8
 #define LAT A3
 #define OE 9
@@ -10,37 +9,50 @@
 #define C A2
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, false);
 
+unsigned short rB = matrix.Color333(0, 0, 7);
+unsigned short dB = matrix.Color333(0, 0, 1);
 unsigned short sB = matrix.Color333(0, 2, 7);
+unsigned short y = matrix.Color333(7, 7, 0);
+unsigned short rC = matrix.Color333(7, 2, 0);
+unsigned short w = matrix.Color333(7, 7, 7);
 
-int c1_8[2] = {6, 14};
-unsigned short cc1_8[2] = {sB, sB};
 
-int c2_3_4_9_10_11[3] = {5, 10, 15};
-unsigned short cc2_3_4_9_10_11[3] = {sB, sB, sB};
+int c0_15[4] = {2, 4, 6, 9};
+unsigned short cc0_15[4] = {dB, sB, rB, dB};
 
-int c5_12[7] = {5, 6, 9, 10, 11, 14, 15};
-unsigned short cc5_12[7] = {sB, sB, sB, sB, sB, sB, sB};
+int c1_14[8] = {1, 3, 4, 5, 6, 7, 8, 9};
+unsigned short cc1_14[8] = {rB, dB, sB, sB, rB, rB, dB, dB};
 
-int c6_13[8] = {6, 7, 8, 9, 11, 12, 13, 14};
-unsigned short cc6_13[8] = {sB, sB, sB, sB, sB, sB, sB, sB};
+int c2_13[6] = {2, 3, 4, 5, 7, 8};
+unsigned short cc2_13[6] = {rB, dB, dB, sB, rB, dB};
 
-int c15_22[5] = {6, 7, 13, 14, 15};
-unsigned short cc15_22[5] = {sB, sB, sB, sB, sB};
+int c3_12[11] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15};
+unsigned short cc3_12[11] = {sB, sB, rB, dB, dB, sB, rB, dB, rB, sB, y};
 
-int c16_23[5] = {5, 6, 12, 13, 15};
-unsigned short cc16_23[5] = {sB, sB, sB, sB, sB};
+int c4[8] = {0, 5, 6, 7, 8, 9, 13, 14};
+unsigned short cc4[8] = {sB, y, dB, rB, dB, rB, rC, y};
 
-int c17_24[4] = {5, 11, 12, 15};
-unsigned short cc17_24[4] = {sB, sB, sB, sB};
+int c5[9] = {4, 5, 7, 8, 9, 12, 13, 14, 15};
+unsigned short cc5[9] = {y, y, dB, dB, rB, rC, rC, y, y};
 
-int c18_25[4] = {5, 10, 11, 15};
-unsigned short cc18_25[4] = {sB, sB, sB, sB};
+int c6[8] = {3, 4, 5, 7, 8, 9, 11, 12};
+unsigned short cc6[8] = {w, w, y, rC, rC, dB, rC, rC};
 
-int c19_26[5] = {5, 6, 9, 10, 15};
-unsigned short cc19_26[5] = {sB, sB, sB, sB, sB};
+int c7[9] = {3, 4, 5, 6, 7, 8, 9, 10, 11};
+unsigned short cc7[9] = {w, sB, w, rC, rC, rC, rC, rC, rC};
 
-int c20_27[5] = {6, 7, 8, 9, 15};
-unsigned short cc20_27[5] = {sB, sB, sB, sB, sB};
+int c8[9] = {3, 4, 5, 6, 7, 8, 9, 10, 11};
+unsigned short cc8[9] = {w, w, w, rC, rC, rC, rC, rC, rC};
+
+int c9[8] = {4, 5, 6, 7, 8, 9, 11, 12};
+unsigned short cc9[8] = {w, w, rC, rC, rC, dB, rC, rC};
+
+int c10[7] = {7, 8, 9, 12, 13, 14, 15};
+unsigned short cc10[7] = {dB, dB, rB, rC, rC, y, y};
+
+int c11[7] = {0, 6, 7, 8, 9, 13, 14};
+unsigned short cc11[7] = {sB, dB, rB, dB, rB, rC, y};
+
 
 void drawCol(int c, int r, int column[], int length, unsigned short color[]) {
   for(int i=0; i<length; i++) {
@@ -48,48 +60,25 @@ void drawCol(int c, int r, int column[], int length, unsigned short color[]) {
   }
 }
 
-void empty() {
-  matrix.fillScreen(matrix.Color333(0, 0, 0));
-}
 
 void setup() {
-  
+  matrix.begin();
 }
 void loop() {
-  matrix.begin();
-  int dR = 0;
-  int del = 40;
-  delay(1000);
-  for(int i=32; i>-28; i--) {
-    drawCol(i, dR, c1_8, 2, cc1_8);
-    drawCol(i+1, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+2, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+3, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+4, dR, c5_12, 7, cc5_12);
-    drawCol(i+5, dR, c6_13, 8, cc6_13);
-    
-    drawCol(i+7, dR, c1_8, 2, cc1_8);
-    drawCol(i+8, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+9, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+10, dR, c2_3_4_9_10_11, 3, cc2_3_4_9_10_11);
-    drawCol(i+11, dR, c5_12, 7, cc5_12);
-    drawCol(i+12, dR, c6_13, 8, cc6_13);
-    
-    drawCol(i+14, dR, c15_22, 5, cc15_22);
-    drawCol(i+15, dR, c16_23, 5, cc16_23);
-    drawCol(i+16, dR, c17_24, 4, cc17_24);
-    drawCol(i+17, dR, c18_25, 4, cc18_25);
-    drawCol(i+18, dR, c19_26, 5, cc19_26);
-    drawCol(i+19, dR, c20_27, 5, cc20_27);
-    
-    drawCol(i+21, dR, c15_22, 5, cc15_22);
-    drawCol(i+22, dR, c16_23, 5, cc16_23);
-    drawCol(i+23, dR, c17_24, 4, cc17_24);
-    drawCol(i+24, dR, c18_25, 4, cc18_25);
-    drawCol(i+25, dR, c19_26, 5, cc19_26);
-    drawCol(i+26, dR, c20_27, 5, cc20_27);
-    
-    delay(del);
-    empty();
-  }
+  drawCol(0, 0, c0_15, 4, cc0_15);
+  drawCol(1, 0, c1_14, 8, cc1_14);
+  drawCol(2, 0, c2_13, 6, cc2_13);
+  drawCol(3, 0, c3_12, 11, cc3_12);
+  drawCol(4, 0, c4, 8, cc4);
+  drawCol(5, 0, c5, 9, cc5);
+  drawCol(6, 0, c6, 8, cc6);
+  drawCol(7, 0, c7, 9, cc7);
+  drawCol(8, 0, c8, 9, cc8);
+  drawCol(9, 0, c9, 8, cc9);
+  drawCol(10, 0, c10, 7, cc10);
+  drawCol(11, 0, c11, 7, cc11);
+  drawCol(12, 0, c3_12, 11, cc3_12);
+  drawCol(13, 0, c2_13, 6, cc2_13);
+  drawCol(14, 0, c1_14, 8, cc1_14);
+  drawCol(15, 0, c0_15, 4, cc0_15);
 }
